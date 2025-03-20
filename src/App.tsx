81dbdf1d93f2534ced6151afc91f6fb39a9cbd1d
Mohit Chaudhari <mohitchaudhari92@gmail.com>
2025-03-20 22:27:17 +0530

    integreate tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 const App = () => {
-  return <h1>Welcome to Fantasy IPL 🏏</h1>;
+  return <div className=" bg-orange-300 h-5 text-x1 font-bold text-red-500">
+  Hello world!
+</div>;
 };
 
 export default App;
